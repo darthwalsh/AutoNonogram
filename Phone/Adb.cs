e7a36adf6fc6669f6030a64d624c09b90813079e
Carl Walsh <darthwalsh@gmail.com>
2020-10-25 10:46:34 -0700

    add untested adb lib

---
+++ Phone/Adb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Phone
+{
+    public static class Adb
+    {
+      public static void Tap(int x, int y) {
+        var psi = new ProcessStartInfo
+        {
+          FileName = @"C:\Carl\scoop\shims\adb.exe",
+          Arguments = $"shell input tap {x} {y}",
+          UseShellExecute = false,
+          // RedirectStandardOutput = true,
+          // RedirectStandardError = true,
+          CreateNoWindow = true,
+        };
+        using (var p = Process.Start(psi)) {
+          p.WaitForExit();
+          if (p.ExitCode != 0) {
+            throw new InvalidOperationException(p.ExitCode.ToString());
+          }
+        }
+      }
+    }
+}
