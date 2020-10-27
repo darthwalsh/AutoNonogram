6ee8e19e7c186f4185b5061fde447528f2b487a0
Carl Walsh <darthwalsh@gmail.com>
2020-10-27 10:24:08 -0700

    TDD Solver

---
+++ Solver/Logic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solver
+{
+  public sealed class Logic
+  {
+    int dim;
+    public Logic(int dim) {
+      this.dim = dim;
+    }
+
+    public void Check(List<Cell> cells) {
+      throw new NotImplementedException();
+    } 
+  }
+
+  public sealed class Cell {
+    public bool? filled;
+  }
+}
